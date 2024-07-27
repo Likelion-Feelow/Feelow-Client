b0754959fcd4b4670611a1c914ca6c95ec4addd3
dong-wook <boogiewooki02@gmail.com>
2024-07-27 23:13:16 +0900

    에러 해결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// ./App.js
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import '../styles/MainPage.css';
+
 
 function MainPage() {
   
