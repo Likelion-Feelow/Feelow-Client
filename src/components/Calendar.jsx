d4eaac8cf7c0459cb3a76f746f365091c5c5eef2
Kyoungho Eom <kheom0512@gmail.com>
2024-07-29 14:29:44 +0900

    22

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,14 +5,7 @@ import styled from "styled-components";
 import moment from 'moment';
 import 'moment/locale/ko';
 
-const theme = {
-    color: {
-      pink: '#ff69b4', // Define your pink color here
-      brown: '#8b4513', // Define your brown color here
-      // Add any other colors you need
-    },
-    // You can add more properties here as needed
-  };
+
 
 const StyledCalender = styled(Calendar)`
 .react-calendar__navigation {
